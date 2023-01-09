f8c133fa754d350f622f248fbbbc1c550436e557
sinanovice <ic22b019@technikum-wien.at>
2023-01-09 19:54:53 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/bic3/SLMp/SlMpApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic3.SLMp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlMpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlMpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtw/bic3/SLMp/SlMpApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic3.SLMp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlMpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
